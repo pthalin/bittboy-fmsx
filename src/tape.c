6c012c1dee3eb71e0ad290148d9af503011c49b7
Patrik Thalin <patrik@thalin.se>
2020-03-19 23:13:04 +0100

    Added lic text

--- src/tape.c
+++ src/tape.c
@@ -1,3 +1,23 @@
+/*
+  This file is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version. 
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  Some parts are based on cas tools by Vincent van Dam (https://github.com/joyrex2001)  
+
+  Authour: pthalin (https://github.com/pthalin/)
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
